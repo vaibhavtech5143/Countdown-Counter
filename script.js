07d3c7df761e0e2d7cb9fc1954deec948d25e61e
Vaibhav Singh <vaibhavsingh2633@gmail.com>
2023-01-05 09:01:44 +0530

    Countdown Counter

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+function countdown() {
+  const newYearDate = new Date('27 Oct, 2022 12:00:00').getTime();
+  const currenDate = new Date().getTime();
+  const diffMilliseconds = (newYearDate - currenDate);
+
+  const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+  // console.log(diffMilliseconds);
+
+  const days = Math.floor(diffMilliseconds / (60 * 60 * 24 * 1000));
+  // console.log(days);
+
+
+  const hours = Math.floor((diffMilliseconds % (60 * 60 * 24 * 1000)) / (1000 * 60 * 60));
+  // console.log(hours);
+
+  const minutes = Math.floor(((diffMilliseconds % (60 * 60 * 1000)) / (1000 * 60)));
+  // console.log(minutes);
+
+  const seconds = Math.floor(((diffMilliseconds % (60 * 1000)) / 1000));
+  //  console.log(seconds);
+
+  // console.log(diffMilliseconds, days, hours, minutes, seconds);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);  
+  secondsEl.innerHTML = formatTime(seconds);
+
+
+
+
+function formatTime(time){
+      return time < 10 ? '0' + time : time;
+
+}
+
+
+
+
+
+}
+
+setInterval("countdown()", 1000);
